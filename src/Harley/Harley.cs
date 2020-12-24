3e912f6210ec6af62e77488f4a3d7197b84b6de7
Taiizor <Taiizor@github.com>
2020-12-24 22:39:53 +0300

    53X Again 5!

--- src/Harley/Harley.cs
+++ src/Harley/Harley.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿#region Imports
+
+using System;
+
+#endregion
+
+// |---------DO-NOT-REMOVE---------|
+//
+//     Creator: Taiizor
+//     Website: www.Taiizor.com
+//     Created: 16.Aug.2020
+//     Changed: 24.Dec.2020
+//     Version: 1.0.0.1
+//
+// |---------DO-NOT-REMOVE---------|
 
 namespace Harley
 {
+    #region Core
+
     public class Harley
     {
         public const string Author = "Taiizor";
     }
+
+    #endregion
 }
