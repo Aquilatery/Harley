8af1a2d75b355bdb5929b159a1a2309349f40fe0
Taiizor <Taiizor@github.com>
2021-03-09 19:13:15 +0300

    53X Again 18!

--- src/Harley_CR/Program.cs
+++ src/Harley_CR/Program.cs
@@ -12,6 +12,9 @@ namespace Harley_CR
         private static void Main()
         {
             Application.EnableVisualStyles();
+#if NETCOREAPP3_1 || NET5_0 || NET6_0
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+#endif
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
